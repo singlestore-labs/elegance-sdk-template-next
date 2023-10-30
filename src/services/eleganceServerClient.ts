659ad8ae21fb0c2521918ccbeb26cece0639d4ed
demenskyi <yaroslav.demenskiy@gmail.com>
2023-10-30 10:54:31 -0700

    env renamed

--- src/services/eleganceServerClient.ts
+++ src/services/eleganceServerClient.ts
@@ -2,9 +2,9 @@ import { createEleganceServerClient } from "@singlestore/elegance-sdk/server";
 
 export const eleganceServerClient = createEleganceServerClient("mysql", {
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.SINGLESTORE_WORKSPACE_HOST,
+    user: process.env.SINGLESTORE_WORKSPACE_USERNAME,
+    password: process.env.SINGLESTORE_WORKSPACE_PASSWORD,
     database: process.env.DB_NAME
   },
   openai: {
